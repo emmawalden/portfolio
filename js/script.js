b3632f263529b67d1aad6205a7dc9e8628df728d
Emma <emmajanewalden@gmail.com>
2020-05-11 21:48:30 -0400

    added new fonts, styled the hamburger menu in css, added skills icons

--- js/script.js
+++ js/script.js
@@ -1,26 +1,9 @@
 const App = {};
 
 App.menu = function () {
-    $(".hamburgerNav").on("click", function () {
-        console.log("hamby");
-        $(".myLinks").toggleClass("menuHide");
-        $(".hamburger").toggleClass("hidden");
-        $(".close").toggleClass("hidden");
-    })
-};
-
-App.menuClick = function () {
-    $(".topNav a").on("click", function () {
-        $(".topNav").toggleClass("menuHide");
-        $(".close").toggleClass("hidden");
-        $(".hamburger").toggleClass("hidden");
-    })
-};
-
+}
 // Initialize function
 App.init = () => {
-    App.menu();
-    App.menuClick();
 }
 
 // Document Ready function
