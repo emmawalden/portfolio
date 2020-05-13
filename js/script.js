425aba735f4d533efa5e8cbb861a28ad7ad6059b
Emma <emmajanewalden@gmail.com>
2020-05-13 19:55:56 -0400

    got hamburger menu to work with jquery, styled projects

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 const App = {};
 
-App.menu = function () {
+App.hamburger = function () {
+    if(screen.width <= 1500) {
+        
+        $(".hamburger").on('click', function(e) {
+            e.preventDefault();
+            $(".myLinks").toggleClass("menuHide");
+            $(".hamburger").toggleClass("hidden");
+            $(".hamburger").toggleClass("show");
+            $(".close").toggleClass("hidden");
+        })
+
+    }
 }
+
+App.menu = function () {
+    if(screen.width <= 1500) {
+        
+        $(".myLinks").on('click', function (e) {
+            e.preventDefault();
+            $(".myLinks").toggleClass("menuHide");
+            $(".close").toggleClass("hidden");
+            $(".hamburger").toggleClass("hidden");
+        })
+
+    }
+}
+
 // Initialize function
 App.init = () => {
+    App.hamburger();
+    App.menu();
 }
 
 // Document Ready function
