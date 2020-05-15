a3248b81fbfb595946c87b09dbc1ce6de02aaeea
Emma <emmajanewalden@gmail.com>
2020-05-14 20:29:31 -0400

    made the hamburger menu tabbable and added escape to close

--- js/script.js
+++ js/script.js
@@ -1,29 +1,44 @@
 const App = {};
 
-App.hamburger = function () {
-        $(".hamburger").click(function(e) {
+App.openMenu = function () {
+        $(".hamburger").on('click', function(e) {
             e.preventDefault();
             $(".myLinks").toggleClass("menuHide");
             $(".hamburger").toggleClass("hidden");
             $(".close").toggleClass("hidden");
-        })
-        
+        })   
     }
 
-App.closeMenu = function () {
-    $(".close").click(function(e) {
-        e.preventDefault();
-        $(".close").toggleClass("hidden");
-        $(".myLinks").toggleClass("menuHide");
-        $(".hamburger").toggleClass("hidden");
-    })
-}
+    
+    App.closeMenu = function () {
+        $(".close").click(function(e) {
+            e.preventDefault();
+            $(".close").toggleClass("hidden");
+            $(".myLinks").toggleClass("menuHide");
+            $(".hamburger").toggleClass("hidden");
+        })
+    }
+
+    // To open/close the hamburger menu with enter/escape
+    App.keyMenu = function () {
+        $(".topNav").on('keyup', function (e) {
+            e.preventDefault();
+            if(e.key === "Enter" || e.key === "Escape") {
+                $(".myLinks").toggleClass("menuHide");
+                $(".hamburger").toggleClass("hidden");
+                $(".close").toggleClass("hidden");
+
+            }
+
+        })
+    }
     
 
 // Initialize function
 App.init = () => {
-    App.hamburger();
+    App.openMenu();
     App.closeMenu();
+    App.keyMenu();
 }
 
 // Document Ready function
